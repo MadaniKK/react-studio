ed911931d3b99ffa2b83e729545cf1d233307b5c
Minghao Chen <qiwen_li@brown.edu>
2022-11-10 21:12:58 -0500

    finished

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from "./components/BakeryItem.js"
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -11,19 +12,42 @@ bakeryData.forEach((item) => {
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
+  const  [cartItems, setCartItems] = useState([])
+  // const  [price, setPrice] = useState(0)
 
+  function addToCart(item){
+    setCartItems([...cartItems, item])
+  }
+
+  function CalculateTotal(){
+    let total = 0;
+    for(let i =0; i < cartItems.length; i++){
+      total += cartItems[i].price
+    }
+    return total
+    
+  }
   return (
     <div className="App">
       <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
+      <div className="Content">
+        <div className="BakeryContainer">
+          {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
+            <BakeryItem item = {item} addToCart = {addToCart}></BakeryItem>
 
-      {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
-      ))}
+          ))}
+        </div>
 
-      <div>
-        <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
+        <div className="Cart">
+          <h2>Cart</h2>
+          {cartItems.map((item,index)=>(
+            <p> {item.name}</p>
+          ))}
+          
+          <h3>Total Price: {CalculateTotal()}</h3>
+        </div>
       </div>
+
     </div>
   );
 }
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,19 @@
+import "./BakeryItem.css";
+import { useState } from "react";
 // TODO: create a component that displays a single bakery item
+function BakeryItem(props){
+    const item = props.item
+    // const name = props.item.name
+    return (
+        <div className="BakeryItem">
+            <img src={item.image}></img>
+            <p>{item.name}</p>
+            <p>{item.description}</p>
+            <p>${item.price}</p>
+            <button onClick={()=>{props.addToCart(item)}}>Add to cart</button>
+
+        </div>
+    );
+}
+
+export default BakeryItem;
